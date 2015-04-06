c21d09b7f108ac922e0767ccbebde9885af72d4d
Hamilton Kibbe <hamilton.kibbe@gmail.com>
2015-04-06 02:52:22 -0400

    Add double-precision polynomial saturator

--- FxDSP/include/PolySaturator.h
+++ FxDSP/include/PolySaturator.h
@@ -15,26 +15,54 @@
 #include <math.h>
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct PolySaturator PolySaturator;
+typedef struct PolySaturatorD PolySaturatorD;
 
 
 PolySaturator*
 PolySaturatorInit(float n);
 
+PolySaturatorD*
+PolySaturatorInitD(double n);
+
 Error_t
 PolySaturatorFree(PolySaturator* Saturator);
+
+Error_t
+PolySaturatorFreeD(PolySaturatorD* Saturator);
 
 Error_t
 PolySaturatorSetN(PolySaturator* saturator, float n);
 
 Error_t
-PolySaturatorProcess(PolySaturator*     saturator,
-                     float*             out_buffer,
-                     const float*       in_buffer,
-                     unsigned           n_samples);
+PolySaturatorSetND(PolySaturatorD* saturator, double n);
+
+Error_t
+PolySaturatorProcess(PolySaturator* saturator,
+                     float*         out_buffer,
+                     const float*   in_buffer,
+                     unsigned       n_samples);
+
+Error_t
+PolySaturatorProcessD(PolySaturatorD*   saturator,
+                      double*           out_buffer,
+                      const double*     in_buffer,
+                      unsigned          n_samples);
+
 
 float
 PolySaturatorTick(PolySaturator* saturator, float in_sample);
 
+double
+PolySaturatorTickD(PolySaturatorD* saturator, double in_sample);
+
+    
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- FxDSP/src/PolySaturator.c
+++ FxDSP/src/PolySaturator.c
@@ -19,6 +19,16 @@ struct PolySaturator
     float n;
 };
 
+
+struct PolySaturatorD
+{
+    double a;
+    double b;
+    double n;
+};
+
+/*******************************************************************************
+ PolySaturatorInit */
 PolySaturator*
 PolySaturatorInit(float n)
 {
@@ -34,6 +44,24 @@ PolySaturatorInit(float n)
     }
 }
 
+PolySaturatorD*
+PolySaturatorInitD(double n)
+{
+    PolySaturatorD* saturator = (PolySaturatorD*)malloc(sizeof(PolySaturatorD));
+    if (saturator)
+    {
+        PolySaturatorSetND(saturator, n);
+        return saturator;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+
+/*******************************************************************************
+ PolySaturatorFree */
 Error_t
 PolySaturatorFree(PolySaturator* saturator)
 {
@@ -44,6 +72,19 @@ PolySaturatorFree(PolySaturator* saturator)
     return NOERR;
 }
 
+Error_t
+PolySaturatorFreeD(PolySaturatorD* saturator)
+{
+    if (saturator)
+    {
+        free(saturator);
+    }
+    return NOERR;
+}
+
+
+/*******************************************************************************
+ PolySaturatorSetN */
 Error_t
 PolySaturatorSetN(PolySaturator* saturator, float n)
 {
@@ -61,6 +102,25 @@ PolySaturatorSetN(PolySaturator* saturator, float n)
 }
 
 Error_t
+PolySaturatorSetND(PolySaturatorD* saturator, double n)
+{
+    if (saturator)
+    {
+        saturator->a = pow(1./n, 1./n);
+        saturator->b = (n + 1) / n;
+        saturator->n = n;
+        return NOERR;
+    }
+    else
+    {
+        return ERROR;
+    }
+}
+
+
+/*******************************************************************************
+ PolySaturatorProcess */
+Error_t
 PolySaturatorProcess(PolySaturator*     saturator,
                      float*             out_buffer,
                      const float*       in_buffer,
@@ -76,9 +136,33 @@ PolySaturatorProcess(PolySaturator*     saturator,
     return NOERR;
 }
 
+Error_t
+PolySaturatorProcessD(PolySaturatorD*   saturator,
+                      double*           out_buffer,
+                      const double*     in_buffer,
+                      unsigned          n_samples)
+{
+    double buf[n_samples];
+    VectorScalarMultiplyD(buf, (double*)in_buffer, saturator->a, n_samples);
+    VectorAbsD(buf, buf, n_samples);
+    VectorPowerD(buf, buf, saturator->n, n_samples);
+    VectorScalarAddD(buf, buf, -saturator->b, n_samples);
+    VectorNegateD(buf, buf, n_samples);
+    VectorVectorMultiplyD(out_buffer, in_buffer, buf, n_samples);
+    return NOERR;
+}
 
+/*******************************************************************************
+ PolySaturatorTick */
 float
 PolySaturatorTick(PolySaturator* saturator, float in_sample)
 {
     return -(powf(fabsf(saturator->a * in_sample), saturator->n) - saturator->b) * in_sample;
 }
+
+double
+PolySaturatorTickD(PolySaturatorD* saturator, double in_sample)
+{
+    return -(pow(fabs(saturator->a * in_sample), saturator->n) - saturator->b) * in_sample;
+}
+
